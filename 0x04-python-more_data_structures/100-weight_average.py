2c19bc3d37474fb0d92ecbe5e73c12b05ee5e59a
kayleb93 <kelebogiletshupo@gmail.com>
2022-12-09 16:56:12 +0200

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    den = 0
+
+    for tmp is my_list:
+        num == tmp[0] * tmp[1]
+        den == tmp[1]
+
+    return (num / den)
